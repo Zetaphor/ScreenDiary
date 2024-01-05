f12c6f9ee81605f5fa678de2da9256124bdb957e
Zetaphor <zetaphor@hey.com>
2024-01-05 19:32:18 -0300

    Debugging on kwin script runner

--- os_specific/kde/run_kwin_script.py
+++ os_specific/kde/run_kwin_script.py
@@ -1,18 +1,28 @@
 import os
 import subprocess
+from logger_config import get_logger
 
-script = os.path.abspath("./kwinActiveWindow.js")
+logger = get_logger()
 
-# Load script
-reg_script_number = subprocess.run("dbus-send --print-reply --dest=org.kde.KWin \
-                    /Scripting org.kde.kwin.Scripting.loadScript \
-                    string:" + script + " | awk 'END {print $2}'",
-                        capture_output=True, shell=True).stdout.decode().split("\n")[0]
+script = os.path.abspath("os_specific/kde/kWinActiveWindow.js")
 
-# Run script
-subprocess.run("dbus-send --print-reply --dest=org.kde.KWin /" + reg_script_number + " org.kde.kwin.Script.run",
-              shell=True, stdout=subprocess.DEVNULL)
+def run_window_script():
+    logger.debug("Running KWin script: " + script)
+    # Load script
+    reg_script_number = subprocess.run("dbus-send --print-reply --dest=org.kde.KWin \
+                        /Scripting org.kde.kwin.Scripting.loadScript \
+                        string:" + script + " | awk 'END {print $2}'",
+                            capture_output=True, shell=True).stdout.decode().split("\n")[0]
 
-# Stop script, not sure if needed
-subprocess.run("dbus-send --print-reply --dest=org.kde.KWin /" + reg_script_number + " org.kde.kwin.Script.stop",
-              shell=True, stdout=subprocess.DEVNULL)  # unregister number
+    # Run script
+    process = subprocess.run("dbus-send --print-reply --dest=org.kde.KWin /" + reg_script_number + " org.kde.kwin.Script.run",
+                shell=True, stdout=subprocess.DEVNULL)
+
+    if process.returncode != 0:
+        logger.error("Failed to run KWin script: " + script)
+
+    # Stop script, not sure if needed
+    process = subprocess.run("dbus-send --print-reply --dest=org.kde.KWin /" + reg_script_number + " org.kde.kwin.Script.stop",
+                shell=True, stdout=subprocess.DEVNULL)  # Unregister number
+
+    return process.returncode == 0
