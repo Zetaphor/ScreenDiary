48629e411f2446f057f8ecfe3963c991f5469ba2
Zetaphor <zetaphor@hey.com>
2024-01-05 19:32:00 -0300

    Move dbus server into main module

--- screendiary.py
+++ screendiary.py
@@ -1,13 +1,34 @@
 import threading
 import os
+import asyncio
 from dotenv import load_dotenv
-from image_processing.capture import process_display
+from processing.capture import process_display
 from logger_config import get_logger
 from database import check_and_initialize_db, add_record
+from util import empty_folder
+from os_specific.kde.run_kwin_script import run_window_script
+from dbus_next.aio import MessageBus
+from dbus_next.service import ServiceInterface, method
 
 load_dotenv()
 logger = get_logger()
 
+USE_DBUS_SERVER = os.getenv('WINDOW_TITLE_METHOD') == 'kde'
+
+class DbusInterface(ServiceInterface):
+    def __init__(self):
+        super().__init__('com.screendiary.bridge')
+
+    @method()
+    def updateActiveWindow(self, resource_name: "s", resource_class: "s", window_type: "s", caption: "s"):
+        logger.debug(f'Active window: {resource_name}, {resource_class}, {window_type}, {caption}')
+
+async def run_dbus_server():
+    bus = await MessageBus().connect()
+    interface = DbusInterface()
+    bus.export('/com/screendiary/bridge', interface)
+    await bus.request_name('com.screendiary.bridge')
+    await bus.wait_for_disconnect()
 
 def debug_reset():
     if bool(int(os.getenv('DEBUG_RESET'))):
@@ -25,7 +46,7 @@ def capture_timer(func, callback):
     result = func()
     callback(result)
 
-def process_display_result(result):
+def save_display_result(result):
     if result is None:
         logger.error('Received empty display result')
     add_record(result['datetime'], result['file_path'], result['ocr_title'], result['ocr_content'], result['url'], result['url_time'], result['url_partial'], result['should_ocr_content'], result['ocr_time'], result['application_name'])
@@ -33,7 +54,16 @@ def process_display_result(result):
 def main():
     debug_reset()
     check_and_initialize_db()
-    capture_timer(process_display, process_display_result)
+    logger.debug('Window title method: ' + os.getenv('WINDOW_TITLE_METHOD'))
+
+    if os.getenv('WINDOW_TITLE_METHOD') == 'kde':
+        # Need to start the timer before dbus
+        capture_timer(run_window_script, lambda val: None)
+        logger.debug('Starting DBus server...')
+        asyncio.run(run_dbus_server())
+
+    elif os.getenv('WINDOW_TITLE_METHOD') == 'ocr':
+        capture_timer(process_display, save_display_result)
 
 if __name__ == '__main__':
     main()
