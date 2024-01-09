44ef84a829d132da4abb11347d88e2bdf69f1905
Zetaphor <zetaphor@hey.com>
2024-01-09 19:15:35 -0300

    Add accordion component

---
+++ frontend_source/src/components/Accordion.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="accordion">
+    <div
+      class="accordion-header cursor-pointer p-4 bg-gray-200"
+      @click="isOpen = !isOpen"
+    >
+      <slot name="header"></slot>
+    </div>
+    <div
+      v-if="isOpen"
+      class="accordion-content p-4 bg-gray-100"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isOpen = ref(false);
+</script>
